54c5fa077e00b0af8078f8a03d6e5f7ce51b6a28
jcmunav63 <jcmunav63@gmail.com>
2023-06-19 16:33:15 -0600

    Edit css & html files to correct alignment of elements; correct eslint errors in JS file.

--- js/mobile.js
+++ js/mobile.js
@@ -1,13 +1,19 @@
 // JavaScript file for Mobile Menu code
 
-const menuBtn = document.querySelector(".menu");
-const overlay = document.querySelector(".overlay");
-const closeBtn = document.querySelector(".toolbar-2");
+const menuBtn = document.querySelector('.menu');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.toolbar-2');
 
-menuBtn.addEventListener("click", function () {
-  overlay.classList.add("show");
+menuBtn.addEventListener('click', () => {
+  overlay.classList.add('show');
 });
 
-closeBtn.addEventListener("click", function () {
-  overlay.classList.remove("show");
+closeBtn.addEventListener('click', () => {
+  overlay.classList.remove('show');
+});
+
+overlay.querySelectorAll('a').forEach((link) => {
+  link.addEventListener('click', () => {
+    overlay.classList.remove('show');
+  });
 });
