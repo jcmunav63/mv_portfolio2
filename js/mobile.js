05e53e8f136f63b1a84cfaef63c7cca43c19f637
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-06-19 19:23:32 +0100

    Implement mobile menu functionality

--- js/mobile.js
+++ js/mobile.js
@@ -1,1 +1,13 @@
 // JavaScript file for Mobile Menu code
+
+const menuBtn = document.querySelector(".menu");
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".toolbar-2");
+
+menuBtn.addEventListener("click", function () {
+  overlay.classList.add("show");
+});
+
+closeBtn.addEventListener("click", function () {
+  overlay.classList.remove("show");
+});
