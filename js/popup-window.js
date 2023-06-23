8031fc4d588656914b16bb8a7235afd2c63f7c85
jcmunav63 <jcmunav63@gmail.com>
2023-06-22 18:05:10 -0600

    Edit JS file to create work section for desktop version.

--- js/popup-window.js
+++ js/popup-window.js
@@ -1,9 +1,8 @@
-const array = [
-  {
+const array = [{
     title1: 'Tonic',
     title2: 'Tonic',
-    imagemov: '"../images/job1.png"',
-    imagedesk: '"../images/job1b.png"',
+    imagemov: './images/job1.png',
+    imagedesk: './images/job1b.png',
     work1: ['CANOPY', 'Back End Dev', '2015'],
     work2: ['CANOPY', 'Back End Dev', '2015'],
     descr1: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
@@ -17,8 +16,8 @@ const array = [
   {
     title1: 'Multi-Post Stories',
     title2: 'Multi-Post Stories',
-    imagemov: '"../images/job2.png"',
-    imagedesk: '"../images/job2b.png"',
+    imagemov: '"./images/job2.png"',
+    imagedesk: '"./images/job2b.png"',
     work1: ['CANOPY', 'Back End Dev', '2015'],
     work2: ['FACEBOOK', 'Full Stack Dev', '2015'],
     descr1: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
@@ -32,8 +31,8 @@ const array = [
   {
     title1: 'Tonic',
     title2: 'FACEBOOK 360',
-    imagemov: '"../images/job3.png"',
-    imagedesk: '"../images/job3b.png"',
+    imagemov: '"./images/job3.png"',
+    imagedesk: '"./images/job3b.png"',
     work1: ['CANOPY', 'Back End Dev', '2015'],
     work2: ['FACEBOOK', 'Full Stack Dev', '2015'],
     descr1: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
@@ -47,8 +46,8 @@ const array = [
   {
     title1: 'Multi-Post Stories',
     title2: 'Uber Navigation',
-    imagemov: '"../images/job4.png"',
-    imagedesk: '"../images/job4b.png"',
+    imagemov: '"./images/job4.png"',
+    imagedesk: '"./images/job4b.png"',
     work1: ['CANOPY', 'Back End Dev', '2015'],
     work2: ['Uber', 'Lead Developer', '2018'],
     descr1: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
@@ -60,91 +59,74 @@ const array = [
   }
 ];
 
-const grid2 = document.querySelector("#grid2");
+const grid = document.querySelector("#grid2");
 
-function createWorkArticle(work) {
-  const article = document.createElement('article');
-  article.classList.add('work1b');
-  article.innerHTML += `
-  <div class="photo1">
-    <img src=${array.imagedesk}/>
-  </div>
-  <div class="">
-    <h3 class="work-text1b poppins">${array.title2}</h3>
-    <ul class="greyProjectButtons">
-      <li class="projectListItems">${array.techstack[0]}</li>
-      <li class="projectListItems">${array.techstack[1]}</li>
-      <li class="projectListItems">${array.techstack[2]}</li>
-      <li class="projectListItems">${array.techstack[3]}</li>
-    </ul>
-    <div class="greenProjectButton">
-      <button class="see-project poppins" id="${array.id}" >See project</button>
+function createJobs() {
+  const div = document.createElement('div');
+  div.classList.add('work1b');
+  div.innerHTML += `
+  <div class="rows">
+    <div class="photo1">
+        <img class="work1b-img grow" alt="work 1" width="" height="" src="images/job3b.png">
     </div>
-  </div>`;
-  grid2.appendChild(article);
-}
+    <div class="work1b">
+        <h2 class="work-text1b poppins">Tonic</h2>
+        <ul class="flex1">
+            <li><p class="canopy poppins">CANOPY</p></li>
+            <li><img class="dot" alt="" width="16px" height="16px" src="images/dot1.png"></li>
+            <li><p class="bed poppins">Back End Dev</p></li>
+            <li><p><img class="dot" alt="" width="16px" height="16px" src="images/dot1.png"></p></li>
+            <li><p class="year poppins">2015</p></li>
+        </ul>
+        <p class="desc2 poppins">A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
+        <ul class="flex2">
+            <li><p class="stack poppins">html</p></li>
+            <li><p class="stack poppins">css</p></li>
+            <li><p class="stack poppins">javascript</p></li>
+        </ul>
+        <nav>
+            <button type="button" class="see-project poppins">See project</button>
+        </nav>
+    </div>
+  </div>
+  `;
+  grid.appendChild(div);
+};
 
-window.onload = array.forEach(createWorkArticle);
+/* <div>
+    <div class="photo2">
+      <h3>A photo here...</h3>
+      <img class="work1-img grow" src='${array.imagedesk}'/>
+      <img class="work1b-img grow" alt="work 1" width="" height="" src="images/job3b.png">
+    </div>
+    <h3 class="work-text1b poppins">${array["title2"]}</h3>
+    <ul class="flex2">
+      <li><p class="canopy poppins">Uber</p></li>
+      <li><img class="dot" alt="" width="16px" height="16px" src="images/dot1.png" /></li>
+      <li><p class="bed poppins">Lead Developer</p></li>
+      <li><p><img class="dot" alt="" width="16px" height="16px" src="images/dot1.png" /></p></li>
+      <li><p class="year poppins">2018</p></li>
+    </ul>
+    <p class="desc2 poppins">${array["work2"]}</p>
+    <ul class="flex2">
+      <li class="stack poppins">HTML</li>
+      <li class="stack poppins">CSS</li>
+      <li class="stack poppins">JavaScript</li>
+      <li class="stack poppins"></li>
+    </ul>
+    <nav>
+      <button class="see-project poppins" id="${array.id}" >See project</button>
+    </nav>
+  </div> */
 
-// document.body.onload = function() {
-//   const grid2 = document.querySelector("#grid2");
-
-
-//   const par = document.createElement('p');
-//   par.innerHTML = "This is a paragraph!";
-//   par.className = 'about poppins';
-//   par.style.textAlign = "center";
-//   grid2.appendChild(par);
-
-//   const par3 = document.createElement('p');
-//   par3.innerHTML = "Screen width is " + screen.width;
-//   par3.className = 'about poppins';
-//   par3.style.textAlign = "center";
-//   grid2.appendChild(par3);
-
-//   const par2 = document.createElement('p');
-//   par2.innerHTML = "This is another paragraph...";
-//   par2.className = 'about poppins';
-//   par2.style.textAlign = "center";
-//   grid2.appendChild(par2);
-
-//   const div1 = document.createElement('div');
-//   div1.classList.add("div1");
-//   const img1 = document.createElement('img');
-//   img1.classList.add("img1");
-
-//   img1.src = '../images/job1b.png';
-//   var put1 = document.getElementsByClassName('img1');
-//   // put1.appendChild(img1);
-//   div1.appendChild(put1);
-//   grid2.appendChild(div1);
-
-
-
-  for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
-  }
-  // img1.innerHTML = `<img src= '../images/job1.png' alt="job card" class="work1-img grow">`;
-  // div1.appendChild(img1);
-  // grid2.appendChild(div1);
-
-  // for (let i = 0; i < array.length; i++) {
-  //   const jobCard = grid2.createElement('article');
-  //   const jobCardDiv = grid2.createElement('div');
-  //   jobCard.className = 'grid-1';
-  //   jobCardDiv.className = 'work1';
-  //   grid2.appendChild(jobCard);
-  //   jobCardDiv.innerHTML = `<img src= ${array[i].imagedesk} alt="job card" class="work1-img grow">
-  //     <h2 class="work-text1 poppins">${array[i].title2}</h2>`;
-  //   grid2.appendChild(jobCardDiv)
-  // }
+window.onload = array.forEach(createJobs);
 
   if (screen.width < 768) {
     const par4 = document.createElement('p');
     par4.innerHTML = "This is another paragraph";
     par4.className = 'about poppins';
     par4.style.textAlign = "center";
-    grid2.appendChild(par4);
+    grid.appendChild(par4);
     
     // const div1 = document.createElement('div');
     // div1.innerHTML = `<img src= ${array[0].imagemov} alt="job card" class="work1-img grow">`;
